c18ee48f8344f6770f53aece8e276b55856e2450
Juan Pablo Moris <108192079+jpmoris@users.noreply.github.com>
2023-05-26 17:43:40 -0300

    Corrección Formulario

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
-const obj = document.getElementById('boton');
-console.log(obj.innerHTML);
-if (obj !== 'null') {
 
-    obj.addEventListener("click", function(event) {
+
+
+const obj = document.getElementById("formulario-whatsapp");
+
+
+if(obj !== null) {
+    obj.addEventListener("submit", (event) => {
         event.preventDefault();
       
     // obtengo los campos
         var nombre = document.getElementById('nombre').value;
         var mensaje = document.getElementById('mensaje').value;
-    
-        console.log(nombre);
-      
+        alert("A continuación vas a ser redirigido a WhatsApp...");
         window.location.href = 'https://api.whatsapp.com/send?phone=5491157556229&text=¡Hola Juan Pablo!%0A%0ASoy ' + nombre + ' y me contacto por ' + mensaje;
       });
 }
